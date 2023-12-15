5e766303d2c5bc9957477df3728507053bf30042
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2023-12-15 23:20:23 +0100

    Update auth.ts

--- plugins/auth.ts
+++ plugins/auth.ts
@@ -1,9 +1,11 @@
 import Api from "~/lib/api";
 
 export default defineNuxtPlugin(async () => {
-  const accessToken = useCookie("accessToken");
   const config = useRuntimeConfig();
-
+  const accessToken = useCookie("accessToken", {
+    domain: config.public.rootDomain,
+});
+  
   if (accessToken.value) {
     Api.accessToken = accessToken.value;
   } else window.location.replace(config.public.authBaseUrl + '?redirect=' + window.location.href);
